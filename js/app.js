0082cca29c4315c3dfd00d4d46f73d98a18fbf1a
muki26 <56950610+muki26@users.noreply.github.com>
2020-11-16 23:40:01 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var popped = 0;
+
+document.addEventListener('mouseover', function(e){
+    
+    if (e.target.className == "balloon"){
+        
+                e.target.style.backgroundColor = "#ededed";
+                e.target.textContent = "POP!";
+                popped++;
+                checkAllPopped();
+    }   
+});
+
+function checkAllPopped(){
+    if (popped === 16){
+        console.log('all popped!');
+        let gallery = document.querySelector('#balloons');
+        let message = document.querySelector('#pop');
+        gallery.innerHTML = '';
+        message.style.display = 'block';
+    }
+};
